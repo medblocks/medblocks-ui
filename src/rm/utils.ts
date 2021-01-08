a6435a0a1b075aebe5e20212e73b4fa312c3542a
sidharth.ramesh <tornadoalert@gmail.com>
2021-01-08 13:32:15 +0530

    added display and DisplayFunction

--- src/rm/utils.ts
+++ src/rm/utils.ts
@@ -66,3 +66,33 @@ export function getLabelOrdinal(value: number, input: Input) :string {
     }
     throw new Error(`Cannot find list in provided input`)
 }
+
+export function sanitizeDisplayFunction(path: string, fn: Function, store: keyValue) :boolean{
+    try {
+        const result = fn(store)
+        if (typeof result != 'boolean'){
+            console.warn(`[${path}] Got a non boolean result from displayFunction. Ignoring function.`)
+            return false
+        } else {
+            return result
+        }
+    } catch (error) {
+        console.error(`[${path}] Error while evaluating displayFunction: ${error}`)
+        return false
+    }
+}
+
+export function sanitizeComputeFunction(path: string, fn: Function, store: keyValue) :string|undefined {
+try {
+    let computed = fn(store)
+    if (typeof computed != 'string') {
+        console.warn(`[${path}] computeFunction did not return string. Ignoring function.`)
+        return
+    } else {
+        return computed
+    }
+} catch (error) {
+    console.error(`[${path}] Error while evaluating computeFunction: ${error}`)
+    return
+}
+}
