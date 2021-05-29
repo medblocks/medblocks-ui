cbbc726fe40f337d4e08063005ad44b891c7e44b
sidharth.ramesh <tornadoalert@gmail.com>
2021-05-29 21:17:13 +0530

    fix: added ism_transition to context exclusion

--- src/medblocks/form/plugins/openEHRFlat.ts
+++ src/medblocks/form/plugins/openEHRFlat.ts
@@ -73,11 +73,15 @@ function toInsertContext(path: string, nonNullPaths: string[]): boolean {
   if (segments.length <= 2) {
     return true;
   }
+  let previousPath: string;
+  previousPath = segments.slice(0, -1).join('/');
   const previousSegment = segments[segments.length - 2];
-  const previousPath = segments.slice(0, -1).join('/');
   // Eg: context/start_time or context/setting
   if (previousSegment === 'context') {
     return true;
+  }
+  if (previousSegment === 'ism_transition') {
+    previousPath = segments.slice(0, -2).join('/');
   }
   // Eg: templates/vitals/body_temperature/time will only return if some templates/vitals/body_temperature/* is defined
   if (nonNullPaths.some(p => p.startsWith(previousPath))) {
