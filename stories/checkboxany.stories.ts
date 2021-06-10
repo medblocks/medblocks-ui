28deeaaed04ac2744b986419097072c340a80145
Karthik Nishanth <karthik.nishanth06@gmail.com>
2021-06-10 19:48:12 +0530

    added checkbox any

---
+++ stories/checkboxany.stories.ts
@@ -0,0 +1,41 @@
+import { html } from 'lit-html';
+import { snippet } from './utils';
+
+export default {
+  title: 'Utility/mb-checkbox-any',
+  component: 'mb-checkbox-any',
+};
+
+const Template = ({ label = '', data = '', disabled = false }) => html`
+  <mb-checkbox-any
+    @mb-checkbox-any=${(e: any) => (data = e.target.data)}
+    .data=${data}
+    .label=${label}
+    .disabled=${disabled}
+  >
+  </mb-checkbox-any>
+`;
+
+export const Base = Template.bind({});
+
+Base.args = {
+  label: '',
+};
+
+snippet(Base, `<mb-checkbox></mb-checkbox>`);
+
+export const WithLabel = Template.bind({});
+
+WithLabel.args = {
+  label: 'Checkbox Label',
+};
+
+snippet(WithLabel, `<mb-checkbox label="Checkbox label"></mb-checkbox>`);
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  label: 'Disabled',
+  disabled: true,
+};
+
+snippet(Disabled, `<mb-checkbox label="Disabled" disabled=true></mb-checkbox>`);
