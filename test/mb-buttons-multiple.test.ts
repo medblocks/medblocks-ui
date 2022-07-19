c9a8a7a533efd1aa7a11cef928ae794eefdb1dc0
ATHULKNAIR <athul2639@gmail.com>
2022-07-19 16:51:32 +0530

    test for button unselecting and mb-select-multiple

--- test/mb-buttons-multiple.test.ts
+++ test/mb-buttons-multiple.test.ts
@@ -6,71 +6,79 @@ import {
   elementUpdated
 } from '@open-wc/testing';
 import { querySelectorAllDeep } from 'query-selector-shadow-dom';
-import MbButtons from '../src/medblocks/codedtext/buttons';
-import '../src/medblocks/codedtext/buttons'
+import MbButtonsMultiple from '../src/medblocks/codedtext/buttons-multiple';
+import '../src/medblocks/codedtext/buttons-multiple'
 import '../src/medblocks/codedtext/option'
 
-describe('MbButtons', () => {
+describe('MbButtons-Multiple', () => {
 
   it('emits data on input', async () => {
-    const mbbuttons = await fixture<MbButtons>(
-      html`<mb-buttons label="Hello there">
+    const mbbuttons = await fixture<MbButtonsMultiple>(
+      html`<mb-buttons-multiple label="Hello there">
         <mb-option value="option1" label="Option 1"></mb-option>
         <mb-option value="option2" label="Option 2"></mb-option>
-      </mb-buttons>`
+      </mb-buttons-multiple>`
     );
     const buttons = querySelectorAllDeep('button')
     expect(buttons).to.length(2)
     buttons[0].click()
     const event1: any = await oneEvent(mbbuttons, 'mb-input');
-    expect(event1.target.data).to.eql({code: 'option1', value: 'Option 1', terminology: 'local'});
-    buttons[1].click()
-    const event2: any = await oneEvent(mbbuttons, 'mb-input');
-    expect(event2.target.data).to.eql({code: 'option2', value: 'Option 2', terminology: 'local'});
+    expect(event1.target.data).to.eql([{code: 'option1', value: 'Option 1', terminology: 'local'}]);
 
     buttons[1].click()
+    const event2: any = await oneEvent(mbbuttons, 'mb-input');
+    expect(event2.target.data).to.eql([{code: 'option1', value: 'Option 1', terminology: 'local'},{code: 'option2', value: 'Option 2', terminology: 'local'}]);
+
+    buttons[0].click()
     const event3: any = await oneEvent(mbbuttons, 'mb-input');
-    expect(event3.target.data).to.eql(undefined);
+    expect(event3.target.data).to.eql([{code: 'option2', value: 'Option 2', terminology: 'local'}]);
   });
 
   it('emits data with correct terminology', async ()=>{
-    const mbbuttons = await fixture<MbButtons>(
-      html`<mb-buttons label="Hello there" terminology="SOME-TERMINOLOGY">
+    const mbbuttons = await fixture<MbButtonsMultiple>(
+      html`<mb-buttons-multiple label="Hello there" terminology="SOME-TERMINOLOGY">
       <mb-option value="option1" label="Option 1"></mb-option>
       <mb-option value="option2" label="Option 2"></mb-option>
-    </mb-buttons>`
+    </mb-buttons-multiple>`
     );
     const buttons = querySelectorAllDeep('button')
     expect(buttons).to.have.length(2)
     buttons[0].click()
     const event1: any = await oneEvent(mbbuttons, 'mb-input');
-    expect(event1.target.data).to.eql({code: 'option1', value: 'Option 1', terminology: 'SOME-TERMINOLOGY'});
+    expect(event1.target.data).to.eql([{code: 'option1', value: 'Option 1', terminology: 'SOME-TERMINOLOGY'}]);
     buttons[1].click()
     const event2: any = await oneEvent(mbbuttons, 'mb-input');
-    expect(event2.target.data).to.eql({code: 'option2', value: 'Option 2', terminology: 'SOME-TERMINOLOGY'});
+    expect(event2.target.data).to.eql([{code: 'option1', value: 'Option 1', terminology: 'SOME-TERMINOLOGY'},{code: 'option2', value: 'Option 2', terminology: 'SOME-TERMINOLOGY'}]);
   })
   it('changes input on setting data', async () => {
-    const mbbuttons = await fixture<MbButtons>(
-      html`<mb-buttons label="Hello there" terminology="SOME-TERMINOLOGY">
+    const mbbuttons = await fixture<MbButtonsMultiple>(
+      html`<mb-buttons-multiple label="Hello there" terminology="SOME-TERMINOLOGY">
       <mb-option value="option1" label="Option 1"></mb-option>
       <mb-option value="option2" label="Option 2"></mb-option>
       <mb-option value="option3" label="Option 3"></mb-option>
-    </mb-buttons>`
+    </mb-buttons-multiple>`
     );
     const buttons = querySelectorAllDeep('button')
     expect(buttons).to.have.lengthOf(3)
-    mbbuttons.data = {code: 'option2', value: 'Option 2', terminology: 'SOME-TERMINOLOGY'}
+    mbbuttons.data = [{code: 'option2', value: 'Option 2', terminology: 'SOME-TERMINOLOGY'}]
     await oneEvent(mbbuttons, 'mb-input')
     await elementUpdated(mbbuttons);
     expect(buttons[0]).to.not.have.class('button--primary')
     expect(buttons[1]).to.have.class('button--primary')
     expect(buttons[2]).to.not.have.class('button--primary')
 
-    mbbuttons.data = {code: 'option3', value: 'Option 3', terminology: 'SOME-TERMINOLOGY'}
+    mbbuttons.data = [{code: 'option3', value: 'Option 3', terminology: 'SOME-TERMINOLOGY'}]
     await oneEvent(mbbuttons, 'mb-input')
     await elementUpdated(mbbuttons);
     expect(buttons[0]).to.not.have.class('button--primary')
     expect(buttons[1]).to.not.have.class('button--primary')
     expect(buttons[2]).to.have.class('button--primary')
+
+    mbbuttons.data = [{code: 'option1', value: 'Option 1', terminology: 'SOME-TERMINOLOGY'},{code: 'option2', value: 'Option 2', terminology: 'SOME-TERMINOLOGY'}]
+    await oneEvent(mbbuttons, 'mb-input')
+    await elementUpdated(mbbuttons);
+    expect(buttons[0]).to.have.class('button--primary')
+    expect(buttons[1]).to.have.class('button--primary')
+    expect(buttons[2]).to.not.have.class('button--primary')
   });
 });
