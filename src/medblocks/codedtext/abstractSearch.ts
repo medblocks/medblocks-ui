8a149d71d8729559fbf9645bb87d17fe78431bec
ATHULKNAIR <athul2639@gmail.com>
2023-11-16 16:38:12 +0530

    search ui fix, content going outside box

--- src/medblocks/codedtext/abstractSearch.ts
+++ src/medblocks/codedtext/abstractSearch.ts
@@ -392,7 +392,7 @@ export default abstract class MbSearchAbstract extends CodedTextElement {
         </sl-input>
         ${this._showResults()
           ? html`
-              <sl-menu style="min-width: 300px;display:flex;">
+              <sl-menu style="min-width:300px; width:fit-content;">
                 ${this.disablesearch ? null : until(this._results())}
                 <slot name="results"></slot>
                 ${this.disablefallback ? null : this._textFallback()}
