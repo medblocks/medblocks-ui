ef9edb79a19864167130c4bea1110b106ea71a9f
neutralboy <leopoorna99@gmail.com>
2023-12-10 11:55:41 +0530

    creating react components

---
+++ index.ts
@@ -0,0 +1,41 @@
+import { MbCountInputEvent } from './src/medblocks/count/count';
+import { MbDateInputEvent } from './src/medblocks/datetime/date';
+import { MbCheckboxInputEvent } from './src/medblocks/boolean/checkbox';
+import { MbButtonsMultipleInputEvent } from './src/medblocks/codedtext/buttons-multiple';
+import { MbButtonsInputEvent } from './src/medblocks/codedtext/buttons';
+import { MbSearchInputEvent } from './src/medblocks/codedtext/search';
+import { MbSearchMultipleInputEvent } from './src/medblocks/codedtext/search-multiple';
+import { MbSelectInputEvent } from './src/medblocks/codedtext/select';
+import { MbCheckboxAnyInputEvent } from './src/medblocks/context/checkboxAny';
+import { MbContextInputEvent } from './src/medblocks/context/context';
+import { MbDurationInputEvent } from './src/medblocks/duration/duration';
+import { MbFormInputEvent } from './src/medblocks/form/form';
+import { MbMultimediaInputEvent } from './src/medblocks/multimedia/multimedia';
+import { MbPercentInputEvent } from './src/medblocks/proportion/percent';
+import { MbProportionInputEvent } from './src/medblocks/proportion/proportion';
+import { MbQuantityInputEvent } from './src/medblocks/quantity/quantity';
+import { MbInputMultipleInputEvent } from './src/medblocks/text/input-multiple';
+import { MbInputInputEvent } from './src/medblocks/text/input';
+import { MbTextSelectInputEvent } from './src/medblocks/text/text-select';
+
+export {
+  MbCountInputEvent,
+  MbDateInputEvent,
+  MbCheckboxInputEvent,
+  MbButtonsMultipleInputEvent,
+  MbButtonsInputEvent,
+  MbSearchInputEvent,
+  MbSearchMultipleInputEvent,
+  MbSelectInputEvent,
+  MbCheckboxAnyInputEvent,
+  MbContextInputEvent,
+  MbDurationInputEvent,
+  MbFormInputEvent,
+  MbMultimediaInputEvent,
+  MbPercentInputEvent,
+  MbProportionInputEvent,
+  MbQuantityInputEvent,
+  MbInputMultipleInputEvent,
+  MbInputInputEvent,
+  MbTextSelectInputEvent,
+};
--- src/react/index.ts
+++ src/react/index.ts
@@ -1,16 +1,15 @@
 import React from 'react';
-import {createComponent} from '@lit/react';
+import { createComponent } from '@lit/react';
 
 import MbBooleanTag from '../medblocks/boolean/checkbox';
-import type {
-  MbCheckboxInputEvent
-} from "../index";
 
 export const MbBoolean = createComponent({
   tagName: 'mb-boolean',
   elementClass: MbBooleanTag,
   react: React,
   events: {
-    onMbInput: 'mb-input'
+    onMbInput: 'mb-input',
+    onMbDisconnect: 'mb-disconnect',
+    onMbConnect: 'mb-connect',
   }
 });
