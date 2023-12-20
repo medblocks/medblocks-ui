a185c41080d74f0e73b22274a22508f37c907855
Pooja Unnikrishnan <poojaunnikrishnan7558@gmail.com>
2023-12-20 19:54:48 +0530

    test react components

--- test/react/index.tsx
+++ test/react/index.tsx
@@ -2,9 +2,34 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { MbBoolean, MbForm, MbInput } from '../../src/react';
+import {
+  MbBoolean,
+  MbForm,
+  MbInput,
+  MbDropDown,
+  MbOption,
+  MbProportion,
+  MbSelect,
+  MbButtonMultiple,
+  MbButton,
+  MbFilter,
+  MbSearchMultiple,
+  MbSearch,
+  MbCheckBox,
+  MbContext,
+  MbCount,
+  MbDate,
+  MbDuration,
+  MbQuantity,
+  MbUnit,
+  MbMultimedia,
+  MbPercent,
+  MbRepeatableHeadless,
+  MbRepeatableSimple,
+  MbInputMultiple,
+  MbTextSelect,
+} from '../../src/react';
 import { MbInputInputEvent } from '../../src/medblocks/text/input';
-
 
 export const TestRoot = () => {
   const logFieldValue = (e: MbInputInputEvent) => {
@@ -12,8 +37,63 @@ export const TestRoot = () => {
   };
   return (
     <MbForm>
-      <MbBoolean label='FIELD LABEL' data={true} path="aql_path" />
-      <MbInput onMbInput={logFieldValue} label='INPUT FIELD' data="hello world" />
+      <MbInput
+        onMbInput={logFieldValue}
+        label="INPUT FIELD"
+        data="hello world"
+      />
+      <MbBoolean label="Boolean" data={true} path="aql_path" />
+      <MbSelect label="Select">
+        <MbOption label="red" value="red"></MbOption>
+        <MbOption label="blue" value="blue"></MbOption>
+      </MbSelect>
+      <MbButtonMultiple label="Multiple button">
+        <MbOption label="red" value="red"></MbOption>
+        <MbOption label="blue" value="blue"></MbOption>
+      </MbButtonMultiple>
+      <MbButton label="Button">
+        <MbOption label="red" value="red"></MbOption>
+        <MbOption label="blue" value="blue"></MbOption>
+      </MbButton>
+      <MbDropDown />
+
+      <MbSearchMultiple label="Multiple search" />
+      <MbSearch terminology="SNOMED-CT" label="Chief Complaints (mb-search)">
+        <MbFilter
+          label="Conditions 1"
+          value="^181000271107 | (finding) | OR <272379006 | Event (event) |"
+        />
+      </MbSearch>
+      <MbCheckBox label="Checkbox" data={true} />
+      <MbContext label="Context" />
+      <MbCount label="Count" />
+      <MbDate label="Date" />
+      <MbDuration label="Duration" />
+      <MbQuantity
+        label="Quantity"
+        default="mm[Hg]"
+        path="blood_pressure/any_event:0/systolic"
+      >
+        <MbUnit unit="mm[Hg]" label="mm[Hg]" min="" max="1000"></MbUnit>
+      </MbQuantity>
+      <MbMultimedia label="Multimedia" />
+      <MbPercent label="Percent" />
+      <MbRepeatableHeadless path="chief_complaints"></MbRepeatableHeadless>
+      <MbRepeatableSimple>
+        <div>
+          <MbInput label="INPUT" data="Hello World" />
+          <MbButton label="Button">
+            <MbOption label="red" value="red"></MbOption>
+            <MbOption label="blue" value="blue"></MbOption>
+          </MbButton>
+        </div>
+      </MbRepeatableSimple>
+      <MbInputMultiple label="Input multiple" />
+      <MbTextSelect label="Text select">
+        <MbOption label="red" value="red"></MbOption>
+        <MbOption label="blue" value="blue"></MbOption>
+      </MbTextSelect>
+      <MbProportion label="proportion" path="proportion" />
     </MbForm>
   );
 };
